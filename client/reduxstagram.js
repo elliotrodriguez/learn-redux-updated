473d8ba7e13a15d2e415cfa91481e230cd3a4fb9
Elliot Rodriguez <elliotrodriguez72@gmail.com>
2018-11-04 06:55:52 -0500

    wire up state and creators with connect

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
-import Main from './components/main';
+import App from './components/app';
 import css from './styles/style.styl';
 import { BrowserRouter, Switch, Route, Router } from 'react-router-dom';
 import Single from './components/single';
@@ -11,12 +11,12 @@ import store from './store';
 render( 
     <Provider store = {store}>
         <BrowserRouter>
-            <Main>
+            <App>
                 <Switch>
                     <Route path='/view/:postId' component={Single}></Route>
                     <Route path='/' component={Photogrid}></Route>
                 </Switch>
-            </Main>
+            </App>
         </BrowserRouter>
     </Provider>,
 
